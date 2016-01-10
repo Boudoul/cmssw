b21d907b02d0957ac3025aabd26292f70f83d1fc
Gaelle <Gaelle.Boudoul@cern.ch>
2016-01-10 14:59:27 +0100

    Paramters for HL-LHC operation at 14TeV

--- IOMC/EventVertexGenerators/python/VtxSmearedParameters_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedParameters_cfi.py
@@ -537,3 +537,23 @@ Nominal5TeVpp2015VtxSmearingParameters = cms.PSet(
     Y0 = cms.double(0.1676),
     Z0 = cms.double(0.6707)
 )
+
+# Paramters for HL-LHC operation at 14TeV
+HLLHCVtxSmearingParameters14TeV = cms.PSet(
+    MeanXIncm = cms.double(0.),
+    MeanYIncm = cms.double(0.),
+    MeanZIncm = cms.double(0.),
+    TimeOffsetInns = cms.double(0.0),
+    EprotonInGeV = cms.double(7000.0),
+    HalfCrossingAngleInurad = cms.double(295.0),
+    CrabAngleCrossingPlaneInurad = cms.double(295.0),
+    CrabFrequencyCrossingPlaneInMHz = cms.double(400.0),
+    NormalizedEmittanceCrossingPlaneInum = cms.double(2.5),
+    BetaStarCrossingPlaneInm = cms.double(0.15),
+    CrabAngleParallelPlaneInurad = cms.double(0.0),
+    CrabFrequencyParallelPlaneInMHz = cms.double(400.0),
+    NormalizedEmittanceParallelPlaneInum = cms.double(2.5),
+    BetaStarParallelPlaneInm = cms.double(0.15),
+    ZsizeInm = cms.double(0.075),
+    BeamProfile=cms.string("Gauss")
+)
